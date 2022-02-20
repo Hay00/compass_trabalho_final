df517e7980098258cf7af85057fae5a0ed57f1fe
AugustoRabelo <augusto.cesar.furtado@gmail.com>
2022-02-20 15:49:23 -0300

    Estrutura básica do microserviço de gerenciamento + configs h2

--- gerenciamento/src/main/java/com/compass/gerenciamento/model/Produto.java
+++ gerenciamento/src/main/java/com/compass/gerenciamento/model/Produto.java
@@ -4,11 +4,38 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull @NotEmpty
+    private String nome;
+    @NotNull @NotEmpty
+    private String descricao;
+    @NotNull @NotEmpty
+    private String tipo;
+    @NotNull
+    private boolean status;
+    
+    
+	public Produto(String nome, String descricao, String tipo, boolean status) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.tipo = tipo;
+		this.status = status;
+	}
+	
+    
 }
