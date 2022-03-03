5133469c62db67f4ee7102a32507e3b77960fc58
Vinicios <38340863+Hay00@users.noreply.github.com>
2022-03-03 14:04:09 -0300

    Estruturando tabelas entre microservices

---
+++ atendimento/src/main/java/com/compass/atendimento/model/ProdutoPedido.java
@@ -0,0 +1,22 @@
+package com.compass.atendimento.model;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProdutoPedido {
+
+	@EmbeddedId
+	ProdutoPedidoKey id;
+}
